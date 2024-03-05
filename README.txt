8bb947185f7a62320857ac4e5353a17ebb1f62e6
Karl Poulson <programmingnpc@gmail.com>
2024-03-05 12:23:59 -0700

    refactor

---
+++ README.txt
@@ -0,0 +1,9 @@
+To generate datasets:
+Run both Preprocessing ipynb files 
+    1. diabetesPreprocesslipynb
+    2. heartDiseasePreprocess.ipynb
+
+Change 'cleveland-challenge.csv' to 'cleveland.csv' in heartDiseasePreprocess.ipynb
+for either the challenge dataset or the normal dataset.
+
+Run 'python3 main.py' to get KNN Scores
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import knnMain
 import os
+import sys
 
-"""
-Perform KNN grid search on specified dataset
-"""
+diabetesPath = os.path.join(os.path.dirname(__file__), 'data', 'diabetes_cleaned.csv')
+clevelandPath = os.path.join(os.path.dirname(__file__), 'data', 'cleveland_cleaned.csv')
 
-heartDiseasePath = os.path.join(os.path.dirname(__file__), 'heartDisease', 'cleveland_cleaned.csv')
-knnMain.runKNN(heartDiseasePath, targetColumn='num')
+# check if cleaned data exists 
+if not os.path.exists(clevelandPath):
+    print("Please run heartDiseasePreprocess.ipynb")
+    sys.exit()    
 
-# diabetesPath = os.path.join(os.path.dirname(__file__), 'diabetes', 'diabetes_preprocessed.csv')
+if not os.path.exists(diabetesPath):
+    print("Please run diabetesPreprocess.ipynb")
+    sys.exit()
+
+print("\nLocated Datasets! Running KNN...")
+
+# Now run the knn model for all datasets
+print("\nRunning KNN for heart disease")
+knnMain.runKNN(clevelandPath, targetColumn='num')
+
+print("\nRunning KNN for diabetes")
 # knnMain.runKNN(diabetesPath, targetColumn='Outcome')
+
